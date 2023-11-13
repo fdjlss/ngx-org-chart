da5df36f9ff3f7b6a9f7689c909097a7aacdcb9c
jorgelss <jorge@carpetres.cl>
2023-11-13 17:33:54 -0300

    update angular

--- projects/ngx-org-chart/src/lib/ngx-org-chart/ngx-org-chart.component.ts
+++ projects/ngx-org-chart/src/lib/ngx-org-chart/ngx-org-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 
 import { INode } from '../node';
 
@@ -9,14 +9,9 @@ import { INode } from '../node';
 })
 export class NgxOrgChartComponent {
 
-  @Input()
   nodes: INode[];
-
-  @Input()
   hasParent = false;
-
-  @Input()
   direction: 'vertical' | 'horizontal' = 'vertical';
 
-  @Output() itemClick = new EventEmitter<INode>();
+  itemClick = new EventEmitter<INode>();
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgxOrgChartModule } from 'ngx-org-chart';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxOrgChartModule } from 'projects/ngx-org-chart/src/public-api';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
+    // BrowserModule,
+    // AppRoutingModule,
     NgxOrgChartModule
   ],
   providers: [],
